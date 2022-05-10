f97f4aa1f5c606c34040140beb8269094c99b102
Pascal Wittmann <pascal.wittmann@red6-es.de>
2022-05-10 23:48:58 +0200

    Get rid of global variable

--- src/hackaru_timeular.py
+++ src/hackaru_timeular.py
@@ -4,6 +4,7 @@ A simple linkage between a Timular cube and Hackaru.
 
 import asyncio
 from datetime import datetime
+from functools import partial
 
 import requests
 import yaml
@@ -17,7 +18,19 @@ SOFTWARE_REVISION_UUID = "00002a28-0000-1000-8000-00805f9b34fb"
 FIRMWARE_REVISION_UUID = "00002a26-0000-1000-8000-00805f9b34fb"
 ORIENTATION_UUID = "c7e70012-c847-11e6-8175-8c89a55d403c"
 
-CURRENT_TASK = None
+
+class State:
+    """Application state"""
+    current_task: str
+
+    config: str
+
+    def __init__(self, config):
+        """Initialize the current task with the task currently active in Hackaru"""
+
+        self.current_task = requests.get(
+            config['endpoint'] + '/working', headers=headers(config)).json()
+        self.config = config
 
 
 def now():
@@ -25,7 +38,7 @@ def now():
     return datetime.utcnow().strftime('%a %B %d %Y %H:%M:%S')
 
 
-def headers():
+def headers(config):
     """Returns the authenticated headers for Hackaru"""
     return {
         "cookie": f"auth_token_id={config['authid']}; auth_token_raw={config['authtoken']}",
@@ -34,7 +47,8 @@ def headers():
     }
 
 
-def callback(sender: int, # pylint: disable=unused-argument
+def callback_with_state(state: State,
+             sender: int,  # pylint: disable=unused-argument
              data: bytearray):
     """Callback for orientation changes of the Timeular cube"""
     assert len(data) == 1
@@ -42,54 +56,43 @@ def callback(sender: int, # pylint: disable=unused-argument
     print(f"Orientation: {orientation}")
 
     if orientation not in range(1, 9):
-        stop_current_task()
+        stop_current_task(state)
         return
 
-    stop_current_task()
-    task = get_task(orientation)
-    start_task(**task)
+    stop_current_task(state)
+    task = get_task(state, orientation)
+    start_task(state, **task)
 
 
-def get_task(orientation: int):
+def get_task(state: State, orientation: int):
     """Retrieve a task for an orientation from the config file"""
-    task = config['mapping'][orientation]
+    task = state.config['mapping'][orientation]
     return {
-        'projectId': config['tasks'][task]['id'],
-        'description': config['tasks'][task]['description']}
+        'projectId': state.config['tasks'][task]['id'],
+        'description': state.config['tasks'][task]['description']}
 
 
-def start_task(projectId: int, description: str):
+def start_task(state: State, projectId: int, description: str):
     """Start a task in Hackaru"""
-    global CURRENT_TASK
     data = f'{{"activity":{{"description":"{description or ""}","project_id":{projectId},"started_at":"{now()}"}}}}'
 
-    resp = requests.post(config['endpoint'], data=data, headers=headers())
+    resp = requests.post(
+        state.config['endpoint'], data=data, headers=headers(state.config))
 
-    CURRENT_TASK = resp.json()
+    state.current_task = resp.json()
 
 
-def stop_current_task():
+def stop_current_task(state: State):
     """Stop a task in Hackaru"""
-    global CURRENT_TASK
-
-    if CURRENT_TASK == None:
+    if state.current_task is None:
         return
 
-    data = f'{{"activity":{{"id":{CURRENT_TASK["id"]},"stopped_at":"{now()}"}}}}'
+    data = f'{{"activity":{{"id":{state.current_task["id"]},"stopped_at":"{now()}"}}}}'
 
-    requests.put(config['endpoint'] + "/" +
-                 str(CURRENT_TASK['id']), data=data, headers=headers())
+    requests.put(state.config['endpoint'] + "/" +
+                 str(state.current_task['id']), data=data, headers=headers(state.config))
 
-    CURRENT_TASK = None
-
-
-def init_current_task():
-    """Initialize the current task with the task currently active in Hackaru"""
-    global CURRENT_TASK
-
-    resp = requests.get(config['endpoint'] + '/working', headers=headers())
-
-    CURRENT_TASK = resp.json()
+    state.current_task = None
 
 
 async def print_device_information(client):
@@ -114,10 +117,12 @@ async def print_device_information(client):
     print(f"Firmware Revision: {''.join(map(chr, firmware_revision))}")
 
 
-async def main(address):
+async def main(state):
     """Main loop listening for orientation changes"""
-    async with BleakClient(address) as client:
+    async with BleakClient(state.config['address']) as client:
         await print_device_information(client)
+
+        callback = partial(callback_with_state, state)
 
         await client.start_notify(ORIENTATION_UUID, callback)
 
@@ -125,6 +130,4 @@ async def main(address):
             await asyncio.sleep(1)
 
 with open('config.yml', 'r') as f:
-    config = yaml.safe_load(f)
-    init_current_task()
-    asyncio.run(main(config['address']))
+    asyncio.run(main(State(yaml.safe_load(f))))

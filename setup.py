e3bad0f576ac44e1bd67a96bd7e5bf5d124a0eb2
Pascal Wittmann <pascal.wittmann@red6-es.de>
2022-05-13 16:02:15 +0200

    Add long_description

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 """" Setup """
+from pathlib import Path
+
 from setuptools import setup
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="hackaru_timular",
     version="0.1.0",
     description="Track your time with the Timular cube and Hackaru",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/pSub/hackaru-timeular",
     author="Pascal Wittmann",
     author_email="github@pascal-wittmann.de",
